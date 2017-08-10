7719e7a2b4003e4f2671f92c14e295e29589282b
e2718281 <jamie.andry@gmail.com>
2017-08-10 15:57:51 -0500

    Here goes nothing...

--- {{cookiecutter.project_slug}}/config/wsgi.py
+++ {{cookiecutter.project_slug}}/config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for project_template project.
+WSGI config for {{ cookiecutter.project_name }} project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,8 +8,14 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+# This allows easy placement of apps within the interior
+# {{ cookiecutter.project_slug }} directory.
+app_path = os.path.dirname(os.path.abspath(__file__)).replace('/config', '')
+sys.path.append(os.path.join(app_path, '{{ cookiecutter.project_slug }}'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.base")
 
--- {{cookiecutter.project_slug}}/manage.py
+++ {{cookiecutter.project_slug}}/manage.py
@@ -3,7 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.base")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
@@ -19,4 +20,11 @@ if __name__ == "__main__":
                 "forget to activate a virtual environment?"
             )
         raise
+    execute_from_command_line(sys.argv)
+
+    # This allows easy placement of apps within the interior
+    # {{ cookiecutter.project_slug }} directory.
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(os.path.join(current_path, '{{ cookiecutter.project_slug }}'))
+
     execute_from_command_line(sys.argv)
