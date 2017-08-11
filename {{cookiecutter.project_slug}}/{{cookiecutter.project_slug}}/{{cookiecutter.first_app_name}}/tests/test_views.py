31b8193fb4ce3ec820823b0154173855c7569f4d
Jamie Andry <jamie.andry@sleeptrc.com>
2017-08-11 13:13:50 -0500

    TemplateUsed changed to 'base.html'

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.first_app_name}}/tests/test_views.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.first_app_name}}/tests/test_views.py
@@ -4,4 +4,4 @@ class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
         response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(response, 'base.html')
